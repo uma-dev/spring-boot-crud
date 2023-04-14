0cdf127d8f4a48ca327501226bb6d18306d9318d
uma-dev <22565959+uma-dev@users.noreply.github.com>
2023-04-14 13:46:31 -0600

    Added maven structure

---
+++ src/test/java/com/umadev/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umadev.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
